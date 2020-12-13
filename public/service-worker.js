6ed793d84f9d8aa2c31238d38c36eab2bf378e5b
Tom Haigh <tmhaigh@gmail.com>
2020-12-13 14:44:26 +1100

    added more files to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,11 @@
 const FILES_TO_CACHE = [
 	"/",
 	"/index.html",
+	"/db.js",
 	"/index.js",
-	"/styles.css"
+	"/styles.css",
+	"/icons/icon-192x192.png",
+	"/icons/icon-512x512.png"
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -20,6 +23,7 @@ self.addEventListener("install", function (evt) {
 	self.skipWaiting();
 });
 
+// activate
 self.addEventListener("activate", function (evt) {
 	evt.waitUntil(
 		caches.keys().then(keyList => {
@@ -39,7 +43,6 @@ self.addEventListener("activate", function (evt) {
 
 // fetch
 self.addEventListener("fetch", function (evt) {
-	// cache successful requests to the API
 	if (evt.request.url.includes("/api/")) {
 		evt.respondWith(
 			caches.open(DATA_CACHE_NAME).then(cache => {
@@ -62,12 +65,11 @@ self.addEventListener("fetch", function (evt) {
 		return;
 	}
 
-	// if the request is not for the API, serve static assets using "offline-first" approach.
-	// see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
 	evt.respondWith(
-		caches.match(evt.request).then(function (response) {
-			return response || fetch(evt.request);
+		caches.open(CACHE_NAME).then(cache => {
+			return cache.match(evt.request).then(response => {
+				return response || fetch(evt.request);
+			});
 		})
 	);
 });
-
